387ede01d7e45fc7f04527d2e2824ffdba7139bb
GEK0256 <harona00@gmail.com>
2016-01-27 19:04:23 -0500

    Finished Card and Deck classes, now on to the game itself.

--- ERS/src/ha/javaers/Deck.java
+++ ERS/src/ha/javaers/Deck.java
@@ -1,5 +1,34 @@
 package ha.javaers;
 
-public class Deck {
+import java.util.Random;
+import java.util.ArrayList;
 
+public class Deck {
+	private ArrayList<Card> cards;	
+	
+	Deck(){
+		cards = new ArrayList<Card>();
+		int index1, index2, size;
+		Random generator = new Random();
+		Card temp;
+		//initializing the deck
+		for(int i = 0; i < 4; i++){
+			for(int j  = 0; j < 14; j++){
+				cards.add(new Card(i, j));
+			}
+		}
+		//shuffling the deck
+		for(int i = 0; i < 100; i++){
+			index1 = generator.nextInt(cards.size() - 1);
+			index2 = generator.nextInt(cards.size() - 1);
+			
+			temp = cards.get(index2);
+			cards.set(index2, cards.get(index1));
+			cards.set(index1, temp);
+		}
+	}
+	
+	public Card drawFromDeck(){
+		return cards.remove(0);
+	}
 }
