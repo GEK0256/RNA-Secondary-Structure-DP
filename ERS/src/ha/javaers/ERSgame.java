79e26753c71960a7873b9cb6e20d950cade69005
GEK0256 <harona00@gmail.com>
2016-01-27 19:18:26 -0500

    Begun writing Hand

--- ERS/src/ha/javaers/ERSgame.java
+++ ERS/src/ha/javaers/ERSgame.java
@@ -1,5 +1,12 @@
 package ha.javaers;
 
 public class ERSgame {
-
+	int numplayers;
+	int[] players;
+	ERSgame(int p){
+		numplayers = p;
+		players = new int[p];
+		for(int i = 0; i  < p; i++){
+		}
+	}
 }
---
+++ ERS/src/ha/javaers/Hand.java
@@ -0,0 +1,17 @@
+package ha.javaers;
+
+public class Hand {
+	private Card[] cards;
+	private int[] value;
+	
+	Hand(Deck d, int p){
+		
+		value = new int[];
+		cards = new Card[];
+		for(int i = 0; i < 5; i++){
+			
+		}
+	}
+	
+	
+}
