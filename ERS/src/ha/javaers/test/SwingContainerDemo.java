d022ae5bcef63695f758a395ed74f66acf34cd68
GEK0256 <harona00@gmail.com>
2016-01-29 00:44:14 -0500

    Testing Jpanel

---
+++ ERS/src/ha/javaers/test/SwingContainerDemo.java
@@ -0,0 +1,20 @@
+package ha.javaers.test;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class SwingContainerDemo {
+	private JFrame mainFrame;
+	private JLabel headerLabel;
+	private JLabel statusLabel;
+	private JPanel controlPanel;
+	private JLabel msglabel;
+	
+	public SwingContainerDemo(){
+		prepareGUI();
+	}
+	
+	
+	
+	
+}
