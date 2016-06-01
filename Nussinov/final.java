9f09da8e61e9205811c319940007bdab2775368b
bestwillcui <bestwillcui@gmail.com>
2016-06-01 14:53:26 -0400

    Update final.java

--- Nussinov/final.java
+++ Nussinov/final.java
@@ -5,15 +5,15 @@ public class SecondaryRNA {
 	public static int maxBasePair(String rna) {
 		int n = rna.length(); // length of RNA sequence
 		int[][] values = new int[n+1][n]; // number of total bonds for each subsequence (DP array)
-		for (int i=0;i<=5;i++) {
-			for (int j=0;j<n;j++) {
+		for (int i = 0; i <= 5; i++) {
+			for (int j = 0; j<n; j++) {
 				values[i][j] = 0; // 0 pairs for length 5 or less (no sharp turns)
 			}
 		}
-		for (int i=6;i<n+1;i++) { // i is the length
-			for (int j=0;j<n-i+1;j++) { // j is the starting position
+		for (int i = 6; i < n+1; i++) { // i is the length
+			for (int j = 0; j < n-i+1; j++) { // j is the starting position
 				int max = values[i-1][j]; // if the last one doesn't pair up
-				for (int k=j;k<i+j-5;k++) { // if the last one pairs up with something
+				for (int k = j; k < i+j-5; k++) { // if the last one pairs up with something
 					if (complement(rna.charAt(k), rna.charAt(i+j-1))) { // Watson-Crick
 						max = Math.max(max, values[k-j][j]+values[i-k+j-2][k+1]+1); // for each pairing of last element with another
 					}
